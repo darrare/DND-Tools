b4b6a11b4814d4719f5b46afff4fe36dfff4e38a
darrare <ryandarras@gmail.com>
2022-01-02 14:15:31 -0700

    Automaton done, but has no function other than simple validation

---
+++ DeterministicFiniteAutomaton/DeterministicFiniteAutomaton/DeterministicFiniteAutomaton.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DeterministicFiniteAutomaton
+{
+    /// <summary>
+    /// This Deterministic Finite Automaton is designed to handle string 
+    /// input and handle it character by character.
+    /// </summary>
+    public class DeterministicFiniteAutomaton
+    {
+        private List<DfaNode> Nodes { get; set; } = new List<DfaNode>();
+
+        public DeterministicFiniteAutomaton(int nodeCount, List<Tuple<int, int, string>> edgeConnections, List<int> acceptStates)
+        {
+            // Create nodes
+            for (int i = 0; i < nodeCount; i++)
+            {
+                Nodes.Add(new DfaNode());
+            }
+
+            // Mark the accept states
+            foreach (var acceptState in acceptStates)
+            {
+                Nodes[acceptState].IsAcceptState = true;
+            }
+
+            // Create edges
+            foreach (var edgeConnection in edgeConnections)
+            {
+                Nodes[edgeConnection.Item1]
+                    .AddExitTransition(Nodes[edgeConnection.Item2], edgeConnection.Item3);
+            }
+        }
+
+        public bool TestStringForValidity(string input)
+        {
+            DfaNode curNode = Nodes[0];
+            int stringIndex = 0;
+            while (stringIndex < input.Length)
+            {
+                // No path to transition on, invalid string
+                if (curNode == null)
+                    return false;
+
+                curNode = curNode.TransitionOnChar(input[stringIndex]);
+                stringIndex++;
+            }
+
+            return curNode.IsAcceptState;
+        }
+
+
+        #region Private classes
+
+        internal class DfaNode
+        {
+            public bool IsAcceptState { get; set; } = false;
+            public List<DfaEdge> ExitTransitions { get; set; } = new List<DfaEdge>();
+
+            public DfaNode()
+            {
+
+            }
+
+            public DfaNode TransitionOnChar(char inputChar)
+            {
+                List<DfaEdge> validTransitions = 
+                    ExitTransitions.Where(t => Regex.IsMatch(inputChar.ToString(), t.RegexTransitionString)).ToList();
+
+
+                switch (validTransitions.Count)
+                {
+                    case 0:
+                        return null;
+                    case 1:
+                        return validTransitions.First().TargetNode;
+                    default:
+                        throw new Exception($"Multiple valid transitions detected for '{inputChar}'... '{string.Join(',', validTransitions.Select(t => t.RegexTransitionString))}'");
+                }
+            }
+
+            public void AddExitTransition(DfaNode target, string regexTransitionString)
+            {
+                ExitTransitions.Add(new DfaEdge(this, target, regexTransitionString));
+            }
+        }
+
+        internal class DfaEdge
+        {
+            public string RegexTransitionString { get; set; }
+            public DfaNode FromNode { get; set; }
+            public DfaNode TargetNode { get; set; }
+            public Action PostTransitionAction { get; set; }
+
+            public DfaEdge(DfaNode fromNode, DfaNode targetNode, string regexTransitionString)
+            {
+                FromNode = fromNode;
+                TargetNode = targetNode;
+                RegexTransitionString = regexTransitionString;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DeterministicFiniteAutomaton/DeterministicFiniteAutomaton/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeterministicFiniteAutomaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nodeCount = 18;
+
+            List<Tuple<int, int, string>> edgeConnections = new List<Tuple<int, int, string>>()
+            {
+                // xdy
+                new Tuple<int, int, string>(0, 1, "[1-9]"),
+                new Tuple<int, int, string>(1, 1, "[0-9]"),
+                new Tuple<int, int, string>(1, 2, "[d]"),
+                new Tuple<int, int, string>(2, 3, "[1-9]"),
+                new Tuple<int, int, string>(3, 3, "[0-9]"),
+
+                // Keeps
+                new Tuple<int, int, string>(3, 4, "[k]"),
+                new Tuple<int, int, string>(4, 5, "[h]"),
+                new Tuple<int, int, string>(4, 7, "[l]"),
+                new Tuple<int, int, string>(5, 6, "[1-9]"),
+                new Tuple<int, int, string>(6, 6, "[0-9]"),
+                new Tuple<int, int, string>(6, 4, "[k]"),
+                new Tuple<int, int, string>(7, 8, "[1-9]"),
+                new Tuple<int, int, string>(8, 8, "[0-9]"),
+                new Tuple<int, int, string>(8, 4, "[k]"),
+
+                // Drops
+                new Tuple<int, int, string>(3, 9, "[d]"),
+                new Tuple<int, int, string>(9, 10, "[h]"),
+                new Tuple<int, int, string>(9, 12, "[l]"),
+                new Tuple<int, int, string>(10, 11, "[1-9]"),
+                new Tuple<int, int, string>(11, 11, "[0-9]"),
+                new Tuple<int, int, string>(11, 9, "[d]"),
+                new Tuple<int, int, string>(12, 13, "[1-9]"),
+                new Tuple<int, int, string>(13, 13, "[0-9]"),
+                new Tuple<int, int, string>(13, 9, "[d]"),
+
+                // Multiplier
+                new Tuple<int, int, string>(3, 14, @"[×x*\/÷]"),
+                new Tuple<int, int, string>(6, 14, @"[×x*\/÷]"),
+                new Tuple<int, int, string>(8, 14, @"[×x*\/÷]"),
+                new Tuple<int, int, string>(11, 14, @"[×x*\/÷]"),
+                new Tuple<int, int, string>(13, 14, @"[×x*\/÷]"),
+                new Tuple<int, int, string>(14, 15, "[1-9]"),
+                new Tuple<int, int, string>(15, 15, "[0-9]"),
+
+                // Additions
+                new Tuple<int, int, string>(3, 16, "[+-]"),
+                new Tuple<int, int, string>(6, 16, "[+-]"),
+                new Tuple<int, int, string>(7, 16, "[+-]"),
+                new Tuple<int, int, string>(11, 16, "[+-]"),
+                new Tuple<int, int, string>(13, 16, "[+-]"),
+                new Tuple<int, int, string>(15, 16, "[+-]"),
+                new Tuple<int, int, string>(16, 17, "[1-9]"),
+                new Tuple<int, int, string>(17, 17, "[0-9]"),
+            };
+
+            List<int> acceptStates = new List<int>() { 3, 6, 8, 11, 13, 15, 17 };
+
+            DeterministicFiniteAutomaton myAuto = new DeterministicFiniteAutomaton(nodeCount, edgeConnections, acceptStates);
+
+            bool result;
+
+
+            // All valid
+            result = myAuto.TestStringForValidity("2d6");
+            result = myAuto.TestStringForValidity("15d20kh5");
+            result = myAuto.TestStringForValidity("15d20kh5");
+            result = myAuto.TestStringForValidity("15d20kl5");
+            result = myAuto.TestStringForValidity("15d20dh5");
+            result = myAuto.TestStringForValidity("15d20dl5");
+            result = myAuto.TestStringForValidity("15d20kh50");
+            result = myAuto.TestStringForValidity("15d20kl50");
+            result = myAuto.TestStringForValidity("15d20dh50");
+            result = myAuto.TestStringForValidity("15d20dl50");
+            result = myAuto.TestStringForValidity("15d20dl5dh5");
+            result = myAuto.TestStringForValidity("15d20dh5dl5");
+            result = myAuto.TestStringForValidity("15d20kl5kh5");
+            result = myAuto.TestStringForValidity("15d20kh5kl5");
+            result = myAuto.TestStringForValidity("15d20dl1dh20");
+            result = myAuto.TestStringForValidity("15d20dh1dl20");
+            result = myAuto.TestStringForValidity("15d20dl20dh1");
+            result = myAuto.TestStringForValidity("15d20dh20dl1");
+            result = myAuto.TestStringForValidity("15d20kl1kh20");
+            result = myAuto.TestStringForValidity("15d20kh1kl20");
+            result = myAuto.TestStringForValidity("15d20kl20kh1");
+            result = myAuto.TestStringForValidity("15d20kh20kl1");
+            result = myAuto.TestStringForValidity("15d20x2");
+            result = myAuto.TestStringForValidity("15d20*2");
+            result = myAuto.TestStringForValidity("15d20/2");
+            result = myAuto.TestStringForValidity("15d20+50");
+            result = myAuto.TestStringForValidity("15d20-50");
+            result = myAuto.TestStringForValidity("15d20/2+2");
+            result = myAuto.TestStringForValidity("15d20/2-2");
+            result = myAuto.TestStringForValidity("15d20x2+2");
+            result = myAuto.TestStringForValidity("15d20x2-2");
+
+            // Some invalid
+            result = myAuto.TestStringForValidity("15d20dh5kl5x");
+            result = myAuto.TestStringForValidity("x15d20kh5dl5");
+            result = myAuto.TestStringForValidity("1x5d20dh5kl5");
+            result = myAuto.TestStringForValidity("15xd20kh5dl5");
+            result = myAuto.TestStringForValidity("15dx20dh5kl5");
+            result = myAuto.TestStringForValidity("15d2x0kh5dl5");
+            result = myAuto.TestStringForValidity("15d20xdh5kl5");
+            result = myAuto.TestStringForValidity("15d20kxh5dl5");
+            result = myAuto.TestStringForValidity("15d20dhx5kl5");
+            result = myAuto.TestStringForValidity("15d20kh5xdl5");
+            result = myAuto.TestStringForValidity("15d20dh5kxl5");
+            result = myAuto.TestStringForValidity("15d20kh5dlx5");
+            result = myAuto.TestStringForValidity("15d20dh5kl5");
+            result = myAuto.TestStringForValidity("15d20kh5dl5");
+            result = myAuto.TestStringForValidity("15d20+50+50");
+            result = myAuto.TestStringForValidity("15d20+50-50");
+            result = myAuto.TestStringForValidity("15d20-50-50");
+        }
+    }
+}
